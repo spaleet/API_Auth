0e79bfb5f993037bfa56ff6a89aa2675ea475c68
Ali M <ali.mo.dev@gmail.com>
2022-09-29 13:13:31 +0330

    Created Entities

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public class User : IdentityUser<Guid>
+{
+    public User()
+    {
+
+    }
+
+    public string SerialNumber { get; set; } = Guid.NewGuid().ToString("N");
+
+    public virtual ICollection<AuthToken> AuthTokens { get; set; }
+}
---
+++ src/Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DatabaseContext : IdentityDbContext<User, UserRole, Guid>
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<AuthToken> AuthTokens { get; set; }
+}
